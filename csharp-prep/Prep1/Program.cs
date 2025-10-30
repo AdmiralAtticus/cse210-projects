2e917e1bc2fc8285b9748d55e6df3e95d472426f
AdmiralAtticus <jackchamberlaindyer@gmail.com>
2025-10-30 09:16:49 -0600

    C# Prep1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("Please enter your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your full name is {firstName} {lastName}");    
     }
 }
