682a112550433b700ee46abe8129c78748304f66
AdmiralAtticus <jackchamberlaindyer@gmail.com>
2025-10-30 19:08:36 -0600

    updated program

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -15,5 +15,20 @@ class Program
         lastName = Console.ReadLine();
 
         Console.WriteLine($"Your full name is {firstName} {lastName}");    
+
+        int x = 10;
+
+        if (!(x == 11 || x == 12 && x == 13 ||!= 23))
+            Console.WriteLine("X is 10");
+
+        else if (x == 435)
+        {
+            Console.WriteLine('Hey Joe');
+        }
+        else
+        {
+            
+        }
+            Console.WriteLine("X is whatever");
     }
 }
