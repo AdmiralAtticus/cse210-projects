1a3efae7d49f2ce9a9e5f21063f8f8dee0674d7c
AdmiralAtticus <jackchamberlaindyer@gmail.com>
2025-11-11 10:00:33 -0700

    Encapsulated data

--- sandbox/CircleApp2/Program.cs
+++ sandbox/CircleApp2/Program.cs
@@ -3,5 +3,18 @@
     static void Main()
     {
         Console.WriteLine("Mag-ingat po tayong lahat");
+        Circle myCircle = new Circle();
+
+        // myCircle._radius = 10;
+        myCircle.SetRadius(10);
+
+        Console.WriteLine(myCircle.GetCircleArea());
+
+        Circle myCircle2 = new Circle(100);
+
+        Console.WriteLine(myCircle2.GetCircleArea());
     }
+
+
+
 }
