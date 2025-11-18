7e3fd33f6221e68b7af481a9862e0b34bab72d40
AdmiralAtticus <jackchamberlaindyer@gmail.com>
2025-11-18 10:01:15 -0700

    first inheritance project

---
+++ sandbox/PersonProject/Doctor.cs
@@ -0,0 +1,13 @@
+class Doctor : Person
+{
+    private string _tools = "";
+    public Doctor(string tools, string firstName, string lastName, int age, int weight) : base(firstName, lastName, age, weight)
+    {
+        _tools = tools;
+    }
+
+    public string DoctorInformation()
+    {
+        return $"{GetPersonInformation()}, Tools: {_tools}";
+    }
+}
